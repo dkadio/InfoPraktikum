be6ff2a7ac71e8ad7d828932cf3e18adae5b0eaa
Christoph Drost <christoph.drost@htw-saarland.de>
2014-07-09 20:45:31 +0200

    An Doku weiter gemacht

--- Routenplaner/Routenplaner.h
+++ Routenplaner/Routenplaner.h
@@ -8,11 +8,13 @@
 #ifndef HELLO_H_
 #define HELLO_H_
 
-#include "./reader/FileOpener.h"
+#include "reader/FileOpener.h"
 #include "geopunkte/LokationsVerwaltung.h"
 #include "graph/Graph.h"
 #include "graph/Dijkstra.h"
 #include "hilfsklassen/UserInterface.h"
+
+
 
 class Hello {
 public:
